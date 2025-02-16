509a3420823c964b55d254517a6193ee38e0ffc6
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-02-16 21:14:23 +0600

    .

---
+++ intro.py
@@ -0,0 +1,23 @@
+#output
+print("hello world")
+
+##variable and array
+name = "Rishat"
+print(name)
+
+cars = ["BMW","HERLEY"]
+print(cars[-2])
+
+# conditions
+
+if 5>10:
+    print('5 is greater than 10')
+    print()
+
+else:
+    print('5 is lower')
+    print('adwodiawohdi')
+    if 3>10:
+        print("2nd block")
+
+    print("1st block")
